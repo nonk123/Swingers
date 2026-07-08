1502975e8c66a48f17a9166b27c7aa0e0e2455b1
nonk123 <me@nonk.dev>
2026-07-08 10:53:29 +0300

    coob

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,14 @@ static void nutblast_raylib_logger(NutBlast_LogLevel level, const char* line) {
     TraceLog(level == NB_LogInfo ? LOG_INFO : LOG_ERROR, "%s", line);
 }
 
+Camera3D camera = {
+    .position = {-5.f, 5.f, -5.f},
+    .target = {0.f, 0.f, 0.f},
+    .up = {0.f, 1.f, 0.f},
+    .fovy = 45.f,
+    .projection = CAMERA_PERSPECTIVE,
+};
+
 int main(int, char*[]) {
     NutBlast_SetGameID(FULL_GAME_TITLE);
     NutBlast_SetLogger(nutblast_raylib_logger);
@@ -31,6 +39,11 @@ int main(int, char*[]) {
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
+
+        BeginMode3D(::camera);
+        DrawCube({0.f, 0.f, 0.f}, 1.f, 1.f, 1.f, RED);
+        EndMode3D();
+
         DrawFPS(5, 5);
         EndDrawing();
     }
